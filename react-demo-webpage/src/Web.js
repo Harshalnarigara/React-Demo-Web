20bb1d5cf53effb6f8f06607ed02694412d2a2ed
Harshalnarigara <harshalnarigara1308@gmail.com>
2024-03-06 22:45:06 +0530

    added componant

---
+++ react-demo-webpage/src/Web.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './Componants/Header'
+import Herosec from './Componants/Hero-sec'
+import Clintsec from './Componants/Clint-sec'
+import About from './Componants/About.js'
+import Why from './Why.js'
+import Skill from './Skill.js'
+import Service from './Service.js'
+import Cta from './Componants/Cta.js'
+import Portfolio from './Componants/Portfolio.js'
+import Team from './Componants/Team.js'
+import Princing from './Componants/Princing.js'
+import Freq from './Componants/Freq.js'
+import Contact from './Componants/Contact.js'
+function Web() {
+  return (
+    <>
+    <Header/>
+    <Herosec/>
+   
+    </>
+  )
+}
+export default Web
